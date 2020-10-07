f82c417483d33799856c2c61f9af3efb121279a2
sbr mgr <sbr.mgr1@gmail.com>
2020-10-07 02:23:30 -0500

    Managed dependency for mockito test

---
+++ product-service/src/test/java/com/product/controllers/ProductControllerTest.java
@@ -0,0 +1,34 @@
+package com.product.controllers;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getProeucts() throws Exception {
+        mockMvc.perform(
+                get("/product/getall")
+        ).andExpect(
+                status().isOk()
+        ).andExpect(
+                jsonPath("$",hasSize(8))
+        ).andExpect(
+                jsonPath("$[0].id", is(8))
+
+        );
+    }
+
+}
