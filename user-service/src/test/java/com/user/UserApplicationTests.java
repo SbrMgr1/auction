efac50745043ab2182d15268711f257be18c1060
sbr mgr <sbr.mgr1@gmail.com>
2020-12-07 09:47:26 -0600

    Added test class resource

--- user-service/src/test/java/com/user/UserApplicationTests.java
+++ user-service/src/test/java/com/user/UserApplicationTests.java
@@ -3,9 +3,11 @@ package com.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ContextConfiguration
+@TestPropertySource(locations = "classpath:application.properties")
 class UserApplicationTests {
 
     @Test
