97206cdb8d062f5b426a38e8b5ffeef8599a4315
sbr mgr <sbr.mgr1@gmail.com>
2020-12-07 09:31:00 -0600

    Test

--- user-service/src/main/java/com/user/UserApplication.java
+++ user-service/src/main/java/com/user/UserApplication.java
@@ -1,12 +1,14 @@
 package com.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class UserApplication {
 
     public static void main(String[] args) {
