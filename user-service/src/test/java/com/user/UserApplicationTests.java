a6a81d5fdd38fb205b33c9e677b903ffed583bdd
sbr mgr <sbr.mgr1@gmail.com>
2020-12-07 09:45:21 -0600

    changed

--- user-service/src/test/java/com/user/UserApplicationTests.java
+++ user-service/src/test/java/com/user/UserApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(classes = UserApplication.class)
+@ContextConfiguration
 class UserApplicationTests {
 
     @Test
