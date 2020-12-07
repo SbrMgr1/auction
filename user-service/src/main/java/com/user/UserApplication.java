85f24ed428a28bf9ec1267dee31649dbc86e7243
sbr mgr <sbr.mgr1@gmail.com>
2020-12-07 09:07:49 -0600

    File changed

--- user-service/src/main/java/com/user/UserApplication.java
+++ user-service/src/main/java/com/user/UserApplication.java
@@ -3,6 +3,7 @@ package com.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
