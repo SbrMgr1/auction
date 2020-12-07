98ef0a945594e41d19d984a82182bf9e56289b53
sbr mgr <sbr.mgr1@gmail.com>
2020-12-07 09:49:41 -0600

    Final test

--- user-service/src/test/java/com/user/AuthenticationTest.java
+++ user-service/src/test/java/com/user/AuthenticationTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.hamcrest.Matchers.*;
@@ -17,32 +18,33 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ContextConfiguration(classes = UserApplication.class)
+@ContextConfiguration
+@TestPropertySource(locations = "classpath:application.properties")
 public class AuthenticationTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//
-//
-//    @Test
-//    public void loginTest() throws Exception {
-//
-//        UserCredential obj = new UserCredential();
-//        obj.setUsername("root");
-//        obj.setPassword("password");
-//        JsonMapper<UserCredential> jsonMapper = new JsonMapper<UserCredential>();
-//
-//        mockMvc.perform(
-//                post("/authenticate")
-//                        .contentType(String.valueOf(MediaType.JSON_UTF_8))
-//                        .content(jsonMapper.toJson(obj))
-//        ).andExpect(
-//                status().isOk()
-//
-//        ).andExpect(
-//                jsonPath("$.status", is(true))
-//        );
-//    }
+    @Autowired
+    private MockMvc mockMvc;
+
+
+
+    @Test
+    public void loginTest() throws Exception {
+
+        UserCredential obj = new UserCredential();
+        obj.setUsername("root");
+        obj.setPassword("password");
+        JsonMapper<UserCredential> jsonMapper = new JsonMapper<UserCredential>();
+
+        mockMvc.perform(
+                post("/authenticate")
+                        .contentType(String.valueOf(MediaType.JSON_UTF_8))
+                        .content(jsonMapper.toJson(obj))
+        ).andExpect(
+                status().isOk()
+
+        ).andExpect(
+                jsonPath("$.status", is(true))
+        );
+    }
 
 }
